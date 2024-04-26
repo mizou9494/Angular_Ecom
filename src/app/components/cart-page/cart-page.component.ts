1aff9fa204d49ea6a658fdc0ab13f74ec31676c1
mizou9494 <betarmizou@gmail.com>
2024-04-26 22:29:02 +0100

    therd version

--- src/app/components/cart-page/cart-page.component.ts
+++ src/app/components/cart-page/cart-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../shared/api.service';
+import { product } from '../product-view/productModal';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css'
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
+  showProduct:any=[];
+  constructor(private api:ApiService) { }
+  ngOnInit(): void {   
+    this.api.products().subscribe(res => {
+      this.showProduct = res;
+      console.log("showProduct" + this.showProduct);
+    })
+  }
+
+  removeItem(item:product) {
+    this.api.removeCartItem(item);
+  }
 
 }
